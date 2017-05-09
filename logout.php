30beb24f9aa1f58989df55bb4c6b5f595e188d6a
yanyufish <alicechanyanyufish@gmail.com>
2017-05-08 20:20:38 -0400

    Update logout.php

--- logout.php
+++ logout.php
@@ -2,22 +2,5 @@
 	session_start();
 	session_unset();
 	session_destroy();
-	$page = <<<PAGE
-<!DOCTYPE html>
-<html lang="en">
-	<head>
-		<title>Logging out</title>
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-		<link rel="stylesheet" href="search.css">
-		<meta http-equiv="refresh" content="3;url=main.html" />
-	</head>
-	
-	<body>
-	Logged out succesfully
-	</body>
-</html>
-PAGE;
-	echo $page;
+	header("Location: main.html");
 ?>
